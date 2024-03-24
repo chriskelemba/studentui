9fb4cafb2c7c02985a3053e8ec49c00631a7ebe1
chriskelemba <loverhxchris@gmail.com>
2024-03-24 23:23:39 +0300

    basic styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom/cjs/react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h2>Student UI</h2>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/RegForm">Register Form</Link>
